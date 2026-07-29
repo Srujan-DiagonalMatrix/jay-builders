f501535a1588c0f1db24d76ce6ef95de7110d72f
Srujan Alikanti <44407185+Srujan-DiagonalMatrix@users.noreply.github.com>
2026-07-29 14:41:23 +0100

    Store JAY logo as text asset

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { enquiryApi } from './src/server/viteEnquiries';
+import { jayLogoAsset } from './src/server/viteJayLogo';
 
-export default defineConfig({ plugins: [react(), enquiryApi()] });
+export default defineConfig({ plugins: [react(), enquiryApi(), jayLogoAsset()] });
